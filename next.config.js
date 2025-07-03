09240e3d8ea1ff99a7ed43ccf128f5cb80e736e4
dap3884 <dap3@iu.edu>
2025-07-02 23:40:38 -0400

    Added CI/CD

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  basePath: isProd ? '/folio-motion' : '',
+  assetPrefix: isProd ? '/folio-motion/' : '',
 };
 
 module.exports = nextConfig;
